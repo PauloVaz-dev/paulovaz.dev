38fdc22a1722e15e6748b5467104a5058d8fe602
Paulo Vaz <psgvaz@gmail.com>
2021-01-08 06:55:16 -0300

    first commit

--- components/Repos/index.js
+++ components/Repos/index.js
@@ -3,7 +3,7 @@ import { GoStar} from 'react-icons/go'
 
 const Repos = ({  repos }) => {
     return(
-        <section className="pt-10 pb-10 contributions" id="contributions">
+        <section className="pt-12 pb-12 contributions" id="contributions">
             <h3 className='about__subtitle text-center text-2xl section-title pb-6 animation_down'>Contribuições</h3>
 
             <div className='pt-8 pl-8 pr-8 bd-grid grid grid-cols-1 md:grid-cols-2 gap-3'>
