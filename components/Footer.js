57b4a86e06b59193cf7d387fed8102c54f7dc175
Paulo Vaz <psgvaz@gmail.com>
2021-01-08 06:38:27 -0300

    first commit

--- components/Footer.js
+++ components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
     return (
         <footer className="footer pt-12 pb-12">
             <div className="bd-grid">
-                <p className="pl-8 pr-8 footer__title text-sm">Esta landing page foi contruido durante Fullstack Master. Tecnologias usadas NextJS + SSR (Server Side Rendering) + Vercel (como plataforma)</p>
-                <p className="pl-8 pr-8 footer__title text-sm">Você pode encontar o codigo fonte desta landing page em:</p>
-                <a href="https://github.com/tuliofaria/tuliofaria.dev">https://github.com/tuliofaria/tuliofaria.dev</a>
+                <p className="pl-8 pr-8 text-sm text-center">Esta landing page foi contruido durante <a target="__blank" href="https://devpleno.com/">Fullstack Master.</a> Tecnologias usadas NextJS + SSR (Server Side Rendering) + Vercel (como plataforma)</p>
+                <p className="pl-8 pr-8 text-sm text-center">Você pode encontar o codigo fonte desta landing page em:</p>
+                <a className="text-center block" href="https://github.com/PauloVaz-dev/paulovaz.dev">https://github.com/PauloVaz-dev/paulovaz.dev</a>
 
-                <p className="pl-8 pr-8">&#169; 2020 copyright all right reserved</p>
+                <p className="pl-8 pr-8 text-center">&#169; 2020 copyright all right reserved</p>
             </div>
         </footer>
 
